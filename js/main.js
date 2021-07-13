de713451504ad1d88bc12f4fef3af12d134819b8
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-07-13 12:01:02 +0530

    Binded inventory to html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 function InventoryViewModel() {
 
     var self = this;
+
+    var iconTypes = [
+        { icon: "icon-bone", text: "Bone"},
+        { icon: "icon-ball", text: "Ball"},
+        { icon: "icon-circle", text: "Circle"},
+        { icon: "icon-rabbit", text: "Rabbit"},
+    ];
+     
+    self.inventory = ko.observableArray([
+        iconTypes[1],
+        iconTypes[2],
+        iconTypes[3],
+    ]);
 }
 
 ko.applyBindings(new InventoryViewModel(), document.querySelector("#knockout-app"));
